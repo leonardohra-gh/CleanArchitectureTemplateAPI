276af660353c9a93a14389e647e7a50dc983b9a6
Leonardo Henrique da Rocha Araujo <136940515+leonardohra-gh@users.noreply.github.com>
2025-03-27 20:42:51 -0300

    Pós atualizações

---
+++ src/Presentation/Controllers/VersionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetVersion()
+        {
+            var version = "0.0.1";
+            return Ok(new { Version = version });
+        }
+    }
+}
